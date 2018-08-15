8e9df2dc1405e259fc09b2dc546b41a4bfbc139b
Lele Shu <leleshu@LHH225MBP.local>
2018-08-14 22:19:15 -0700

    add new function.

---
+++ R/readTSD.R
@@ -0,0 +1,40 @@
+#============ 
+#' Read the time-series file, which has c(nrow, ncol, timestring) in header
+#' \code{read.tsd} 
+#' @param file full path of file
+#' @return xts
+#' @export
+read.tsd <- function(file){
+  # file=fin['md.lai']
+  text=readLines(file)
+  r0 = 1
+  nrow = length(text)
+  xl = list()
+  for(i in 1: 100){
+    header = utils::read.table(text = text[r0] )
+    nr = as.numeric(header[1])
+    nc = as.numeric(header[2])
+    t0 = header[3]
+    tmp =  as.matrix(utils::read.table(text = text[0:nr + 1 + r0], header = T))
+    tsd = xts::as.xts(tmp[,-1], 
+                      order.by = as.POSIXct(t0, "%Y%m%d") + t0 * 86400)
+    xl[[i]] =
+    r0 = r0 + nr + 2;
+    if(r0 + 1 > nrow){
+      break
+    }
+  }
+  xl
+}
+#============ 
+#' Read the .lai Time-Series file
+#' \code{readlai} 
+#' @param file full path of file
+#' @return list of Time-Series data
+#' @export
+readlai <- function( file = PIHM.filein()['lai'] ){
+  x=read.tsd(file)
+  names(x) = c('LAI','RL')
+  x
+}
+
---
+++ R/readinput.R
@@ -0,0 +1,130 @@
+#' Read the matrix or data.frame file which has (nrow, ncol) in header
+#' \code{read.df} 
+#' @param file full path of file
+#' @param text text return from readLines(file)
+#' @return a list of matrix
+#' @export
+read.df <-function(file, text = readLines(file)){
+  # fn=fin['md.mesh']
+  # text=readLines(fn)
+  r0 = 1
+  nrow = length(text)
+  xl = list()
+  for(i in 1: 100){
+    ndim = as.numeric(utils::read.table(text = text[r0] ))
+    nr = ndim[1]
+    nc = ndim[2]
+    xl[[i]] = as.matrix(utils::read.table(text = text[0:nr + 1 + r0], header = T))
+    r0 = r0 + nr + 2;
+    if(r0 + 1 > nrow){
+      break
+    }
+  }
+  xl
+}
+#' Read the .mesh file
+#' \code{readmesh} 
+#' @param file full path of .mesh file
+#' @return pm \code{PIHM.mesh}
+#' @export
+readmesh <-function(file = PIHM.filein()['md.mesh'] ){
+  d = read.df(file = file);
+  ret <- PIHM.mesh(mesh = d[[1]], point = d[[2]])
+}
+
+#' Read the .att file
+#' \code{readmesh} 
+#' @param file full path of file
+#' @return matrix
+#' @export
+readatt <-function( file = PIHM.filein()['md.att']){
+  x = read.df(file)
+  x
+}
+
+#' Read the .riv file
+#' \code{readriv} 
+#' @param file full path of .riv file
+#' @return pr \code{PIHM.riv}
+#' @export
+readriv <-function(file = PIHM.filein()['md.riv'] ){
+  d = read.df(file = file);
+  ret <- PIHM.river(river = d[[1]], 
+                    rivertype = d[[2]],
+                    point = d[[3]])
+}
+
+#' Read the .rivchn file
+#' \code{readchannel} 
+#' @param file full path of .rivchn file
+#' @return matrix
+#' @export
+readchannel <-function(file = PIHM.filein()['md.chn'] ){
+  d = read.df(file = file);
+  ret <- d
+}
+
+#============
+#' Read the .para or .calib file
+#' \code{readpc} 
+#' @param file full path of file
+#' @return .para or .calib
+#' @export
+readpc <- function(file = PIHM.filein()['md.para'] ){
+  # file = fin['md.para']
+  tline = readLines(file, skipNul = TRUE)
+  tmp = which(grepl('[:Alpha:]', tline) | grepl('[:alpha:]', tline))
+  
+  value = as.data.frame( utils::read.table(text = tline, header=FALSE, row.names= 1) )
+  para = unlist(value)
+  names(para) = toupper(rownames(value))
+  para
+}
+
+
+#============
+#' Read the .ic file
+#' \code{readic} 
+#' @param file full path of file
+#' @return .ic
+#' @export
+readic <- function(
+  file = PIHM.filein()['md.ic']){
+  # file = fin['md.ic']
+  x=read.df(file)
+  cn = c('minit', 'rinit', 'linit')
+  names(x) = cn[1:length(x)]
+  x
+}
+
+#' Read the .soil file
+#' \code{readsoil} 
+#' @param file full path of file
+#' @return .soil
+#' @export
+readsoil <-function(file =  PIHM.filein()['md.soil']){
+  x=read.df(file)
+  x
+}
+
+#============
+#' Read the .geol file
+#' \code{readgeol} 
+#' @param file full path of file
+#' @return .geol
+#' @export
+readgeol <-function(file =  PIHM.filein()['md.geol']){
+  x=read.df(file)
+  x
+}
+
+#============ 
+#' Read the .lc file
+#' \code{readlc} 
+#' @param file full path of file
+#' @return .lc
+#' @export
+readlc <-function( file = PIHM.filein()['md.lc']){
+  x=read.df(file)
+  x
+}
