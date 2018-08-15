05ea14bbc0e29e8ba06d1264aaa3748924ba51d3
Lele Shu <leleshu@campus-055-004.ucdavis.edu>
2018-08-15 12:20:31 -0700

    new version reading output file.

--- R/readout.R
+++ R/readout.R
@@ -1,18 +1,17 @@
 #' Read output file from PIHM
 #' @param keyword keyword of the output file. File name format is: projectname.keyword.dat
-#' @param theFile  Full path of output file. 
+#' @param file  Full path of output file. 
 #' @param path path of the outputfile
-#' @param start Time of the first row. 
 #' @keywords read output. Could be used for reading mesh and river format.
 #' @return A TimeSeries data. This list require support of xts packages.
 #' @export  
 readout <- function(keyword,
                     path=get('outpath', envir=.pihmgis) ,
-                    theFile = file.path(path, paste0(get('PRJNAME', envir=.pihmgis),'.', keyword,'.dat') ) ,
-                    start=as.Date(get('STARTDATE', envir=.pihmgis)) 
+                    file = file.path(path, paste0(get('PRJNAME', envir=.pihmgis),'.', keyword,'.dat') ) 
                     ){
-  fid=file(theFile, 'rb');
+  fid=file(file, 'rb');
   nc=readBin(fid, what=integer(), n=1)
+  st=readBin(fid, what=integer(), n=1, size = 8) #Long integer
   tmp=readBin(fid, what=numeric(), n=1e9, size=8)
   close(fid)
   nrr = length(tmp)/(nc+1)
@@ -21,7 +20,7 @@ readout <- function(keyword,
     message('File may not completed. ', nrr, "X", nc+1)
   }
   mat=t(matrix(tmp[1:( nr*(nc+1) )], nrow=nc+1))
-  xt = start + lubridate::minutes(mat[,1])
+  xt = as.POSIXct(as.character(st), format='%Y%m%d') + mat[,1] * 60
   tsd = xts::as.xts(mat[,-1], order.by = xt)
   tsd
 }
