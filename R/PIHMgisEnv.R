8d3900ec5f09aa1661e9866cbedf09e7995e6d72
Lele Shu <leleshu@LHH225MBP.local>
2018-08-16 22:32:27 -0700

    change the .pihmgis env to .pihm env

--- R/PIHMgisEnv.R
+++ R/PIHMgisEnv.R
@@ -1,12 +1,19 @@
 #============ 
-#' Setup .pihmgis environment.
+#' Setup .pihm environment.
 #' \code{read.tsd} 
 #' @param inpath path of PIHM input directory
 #' @param outpath path of PIHM output directory
 #' @param prjname charactor of PIHM project name.
+#' @param anapath charactor of PIHM project name.
 #' @export
-PIHM <- function(prjname, inpath = './', outpath = './'){
-  assign('PRJNAME', prjname, envir=.pihmgis)
-  assign('inpath', inpath, envir=.pihmgis)
-  assign('outpath', outpath, envir=.pihmgis)
+PIHM <- function(prjname, inpath = './', outpath = './', 
+                 anapath = file.path(outpath, 'PIHManalysis')){
+  assign('PRJNAME', prjname, envir=.pihm)
+  assign('inpath', inpath, envir=.pihm)
+  assign('outpath', outpath, envir=.pihm)
+  
+  dir.create(anapath, showWarnings = F, recursive = T)
+  assign('anapath', anapath, envir = .pihm)
+  
+  assign('PIHM.MASK', NULL, envir=.pihm)
 }
