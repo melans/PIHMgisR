de6799a06c10601c682a3d85d58a489f4bd273ee
Lele Shu <leleshu@campus-014-000.ucdavis.edu>
2018-09-06 14:23:00 -0700

    add examples.

--- R/writeShape.R
+++ R/writeShape.R
@@ -3,7 +3,16 @@
 #' @param shp Spatial file
 #' @param crs projection 
 #' @param file file path, without '.shp'.
-writeshape <- function(shp,crs = raster::crs(shp), file=NULL){
+#' @examples 
+#' library(sp)
+#' library(rgeos)
+#' library(rgdal)
+#' sp1 = readWKT("POLYGON((0 0, 0 1, 1 1, 1 0, 0 0))")
+#' raster::crs(sp1) =sp::CRS("+init=epsg:4326")
+#' writeshape(sp1, file=file.path(tempdir(), 'sp1'))
+#' sp2=readOGR(file.path(tempdir(), 'sp1.shp'))
+#' plot(sp2)
+writeshape <- function(shp, file=NULL, crs = raster::crs(shp)){
   if(grepl(class(shp)[1],'SpatialPolygons' ) ){
     shp = methods::as(shp, "SpatialPolygonsDataFrame")
   }else if ( grepl(class(shp)[1],'SpatialLines' )   ){
