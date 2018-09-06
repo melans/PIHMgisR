1c3c000c2ef4a0fae784c92be2562dd0c5e7cbfe
Lele Shu <leleshu@campus-014-000.ucdavis.edu>
2018-09-06 14:25:36 -0700

    add removeholes and fishnet.

--- R/FunctionGIS.R
+++ R/FunctionGIS.R
@@ -95,3 +95,106 @@ MeshData2Raster <- function(x=getElevation(),
   ret <- raster::mask(r,mask)
 }
 
+
+#' Remove the holes in polygons
+#' \code{removeholes} 
+#' @param sp SpatialPolygons or SpatialPolygonDataFrame
+#' @return Raster map
+#' @export
+#' @examples 
+#' library(sp)
+#' p.out = Polygon(cbind(c(4,4,6,7,4),c(5,3,2,5,5))  )
+#' p.hole = Polygon(cbind(c(5,6,6,5,5),c(4,4,3,3,4) ), hole = TRUE)
+#' sp <- SpatialPolygons(list(Polygons(list(p.out, p.hole), "1")))
+#' s = removeholes(sp)
+#' par(mfrow=c(1,2))
+#' plot(sp)
+#' plot(s)
+removeholes <- function(sp){
+  x = sp
+  nx = length(x)
+  # vn = rownames(x@data)
+  rl = list()
+  for(i in 1:nx){
+    spg = x@polygons[[i]]@Polygons
+    npg = length(spg)
+    ypg = list()
+    k=1
+    for(j in 1:npg){
+      if(spg[[j]]@hole){
+      }else{
+        ypg[[k]] = spg[[j]]
+        k = k+1
+      }
+    }
+    rl[[i]] = sp::Polygons(ypg, ID=i)
+  }
+  ysp = sp::SpatialPolygons(rl)
+  # ret = SpatialPolygonsDataFrame(ysp, data=x@data)
+  ret = ysp
+  if( !is.na(raster::crs(x)) & ! is.null(raster::crs(x)) ){
+    raster::crs(ret) = raster::crs(x)
+  }
+  ret
+}
+#' Generatue fishnet
+#' \code{fishnet} 
+#' @param ext Extension of the fishnet. c(xmin, xmax, ymin, ymax)
+#' @param dx Interval of x direction
+#' @param dy Interval of y direction
+#' @param crs Projection 
+#' @param polygons Whether to export SpatialPolygons
+#' @param points Whether to export SpatialPoints
+#' @param lines Whether to export SpatialLines
+#' @examples
+#' library(raster)
+#' pg=fishnet(ext=c(-80,80, -50,50), dx=5)
+#' plot(pg)
+fishnet <- function(ext, crs=sp::CRS("+init=epsg:4326"), dx=1, dy=dx,
+                    lines=FALSE, polygons=TRUE, points=FALSE){
+  xmin = ext[1]
+  xmax = ext[2]
+  ymin = ext[3]
+  ymax = ext[4]
+  xx=seq(ext[1], ext[2], by=dx)
+  yy=seq(ext[3], ext[4], by=dy)
+  nx = length(xx)
+  ny = length(yy)
+  if(lines){
+    vline=cbind(xx, ymin, xx, ymax)
+    hline =  cbind(xmin, yy, xmax, yy)
+    mat = rbind(vline, hline)
+    str=paste(paste('(', mat[,1],mat[,2],',', mat[,3], mat[,4], ')'), collapse = ',')
+    spl=rgeos::readWKT(paste('MULTILINESTRING(', str, ')'))
+    df = as.data.frame(mat)
+    colnames(df) = c('x1', 'y1', 'x2','y2')
+    spdf=sp::SpatialLinesDataFrame(spl, data = df)
+    ret = spdf
+  }
+  if(polygons){
+    xy=expand.grid(xx,yy)
+    xm = matrix(xy[,1], nx,ny)
+    ym = matrix(xy[,2], nx, ny)
+    xloc=abind::abind(xm[-nx, -ny], xm[-nx, -1], xm[-1, -1], xm[-1, -ny], xm[-nx, -ny], along=3)
+    yloc=abind::abind(ym[-nx, -ny], ym[-nx, -1], ym[-1, -1], ym[-1, -ny], ym[-nx, -ny], along=3)
+    pgs=list()
+    df=as.data.frame(matrix(0, nrow=(nx-1)*(ny-1), 6))
+    colnames(df) = c('xmin','xmax','ymin', 'ymax','xcenter','ycenter')
+    k=0
+    for(i in 2:nx - 1){
+      for(j in 2:ny - 1){   
+        k=k+1
+        ll = cbind(xloc[i,j, ], yloc[i,j, ])
+        pg=sp::Polygon(ll)
+        pgs[[k]] = sp::Polygons(list(pg), ID=k)
+        df[k,]=c(min(ll[,1]), max(ll[,1]), min(ll[,2]), max(ll[,2]), mean(ll[,1]), mean(ll[,2]) )
+      }
+    }
+    pg = sp::SpatialPolygons(pgs)
+    pg.df = sp::SpatialPolygonsDataFrame(pg, data=df)
+    ret = pg.df
+  }
+  # plot(ret, axes=T)
+  raster::crs(ret) = crs
+  ret
+}
