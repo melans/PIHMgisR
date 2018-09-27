45f5a0f1aa5f91caf28560e18d192e5250a78573
Lele Shu <leleshu@LHH225MBP.local>
2018-09-26 20:19:15 -0700

    update the GreenAmpt function.

--- R/GreemAmpt.R
+++ R/GreemAmpt.R
@@ -1,29 +1,37 @@
 # 
 #' Calculate infiltration with Green Ampt method
 #' \code{GreenAmpt} 
-#' @param x  Input data
-#' @param filter Data filter
-#' @param plot Whether plot the 
-#' @return Matrix information, c('ID','Vmin','Vmax', 'Filter')
+#' @param theta  Soil moisture content
+#' @param thetas  Saturated moisture content
+#' @param ksat  Saturated hydraulic conductivity
+#' @param Hfront Depth of wetting front.
+#' @return Infiltration rate 
 #' @references Green, W.H. and G. Ampt. 1911. Studies of soil physics, part I – the flow of air and water through soils. J. Ag. Sci. 4:1-24.
 #' @export
 #' @examples 
-#' p = sin( (1:100) / 10 )
-#' p[p<0] = 0
-GreenAmpt <- function(t, ts, ks, hf){
-  dt = ts - t
-  dh = hf
-  if(dh <=0 ){
-    dh = 1e-6
-  }
+#' hf= (1:200) / 100
+#' ksat=0.2
+#' theta = matrix(seq(0.1,0.4, by=0.05), ncol=1)
+#' q_inf=apply(theta,1, FUN=function(x) GreenAmpt(x, 0.5, ksat, hf))
+#' col= topo.colors(length(theta) )
+#' lty = 1:length(theta) 
+#' matplot(type='l', q_inf, -hf, col=col, lty=lty, lwd=2)
+#' legend('bottomleft', paste('Theta =', theta), col=col, lty=lty, lwd=2); grid()
+#' abline(v=ksat, lwd=3, lty=2)
+
+GreenAmpt <- function(theta, thetas, ksat, Hfront){
+  dt = thetas - theta
+  dh = Hfront
+  dh[dh <= 0 ] = 1e-6
   if(dt <= 0){
     q = 0
   }else{
-    d0 = ks * dh / dt
+    d0 = ksat * dh / dt
     q = dt * sqrt(d0 / 2 / 1)
   }
   q
 }
+
 # 
 # func  <- function(p, ks=0.1, thetas = 0.5, def=10){
 #   np = length(p)
