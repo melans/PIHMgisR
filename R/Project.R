bad6c079d4116edac71904e7c30f86d302e13fd4
Lele Shu <lele.shu@gmail.com>
2019-11-08 23:01:36 -0800

    update PIHM.filein().

--- R/Project.R
+++ R/Project.R
@@ -1,41 +1,42 @@
 #' Prepare Input file of PIHM
 #' \code{PIHM.filein} 
 #' @param projname Character, project name, default= PRJNAME which is a global variable.
-#' @param indir PIHM input directory, default = inpath which is global variable
+#' @param inpath PIHM input directory, default = inpath which is global variable
+#' @param outpath PIHM output directory, default = outpath which is global variable
 #' @return Character of full path of input files for PIHM
 #' @export
 PIHM.filein <- function(projname=get('PRJNAME', envir=.pihm),
-                        indir = get('inpath', envir=.pihm) ){
-  dir.create(indir, showWarnings = FALSE, recursive = TRUE)
-  fn.mesh = file.path(indir, paste0(projname, '.sp.mesh' ) );
-  fn.att = file.path(indir, paste0(projname, '.sp.att' ) );
-  fn.pt = file.path(indir, paste0(projname, '.sp.points' ) )
-  fn.edge = file.path(indir, paste0(projname, '.sp.edges' ) )
-  fn.mdriv = file.path(indir, paste0(projname, '.sp.riv' ) )
-  fn.mdseg = file.path(indir, paste0(projname, '.sp.rivchn' ) )
+                        inpath = get('inpath', envir=.pihm), 
+                        outpath = get('outpath', envir=.pihm)
+                        ){
+  dir.create(inpath, showWarnings = FALSE, recursive = TRUE)
+  fn.mesh = file.path(inpath, paste0(projname, '.sp.mesh' ) );
+  fn.att = file.path(inpath, paste0(projname, '.sp.att' ) );
+  fn.pt = file.path(inpath, paste0(projname, '.sp.points' ) )
+  fn.edge = file.path(inpath, paste0(projname, '.sp.edges' ) )
+  fn.mdriv = file.path(inpath, paste0(projname, '.sp.riv' ) )
+  fn.mdseg = file.path(inpath, paste0(projname, '.sp.rivchn' ) )
   
-  fn.ic = file.path(indir, paste0(projname, '.cfg.ic' ) )
-  fn.para = file.path(indir, paste0(projname, '.cfg.para' ) )
-  fn.calib = file.path(indir, paste0(projname, '.cfg.calib' ) )
+  fn.ic = file.path(inpath, paste0(projname, '.cfg.ic' ) )
+  fn.para = file.path(inpath, paste0(projname, '.cfg.para' ) )
+  fn.calib = file.path(inpath, paste0(projname, '.cfg.calib' ) )
   
-  fn.soil = file.path(indir, paste0(projname, '.para.soil' ) )
-  fn.geol = file.path(indir, paste0(projname, '.para.geol' ) )
-  fn.lc = file.path(indir, paste0(projname, '.para.lc' ) )
+  fn.soil = file.path(inpath, paste0(projname, '.para.soil' ) )
+  fn.geol = file.path(inpath, paste0(projname, '.para.geol' ) )
+  fn.lc = file.path(inpath, paste0(projname, '.para.lc' ) )
   
-  fn.forc = file.path(indir, paste0(projname, '.tsd.forc' ) )
-  fn.bc = file.path(indir, paste0(projname, '.tsd.bc' ) )
-  fn.lai = file.path(indir, paste0(projname, '.tsd.lai' ) )
-  fn.rl = file.path(indir, paste0(projname, '.tsd.rl' ) )
-  fn.mf = file.path(indir, paste0(projname, '.tsd.mf' ) )
-  outdir = file.path('output', paste0(projname, '.out'))
-  gisdir = file.path(indir, 'gis')
+  fn.forc = file.path(inpath, paste0(projname, '.tsd.forc' ) )
+  fn.bc = file.path(inpath, paste0(projname, '.tsd.bc' ) )
+  fn.lai = file.path(inpath, paste0(projname, '.tsd.lai' ) )
+  fn.rl = file.path(inpath, paste0(projname, '.tsd.rl' ) )
+  fn.mf = file.path(inpath, paste0(projname, '.tsd.mf' ) )
+  gisdir = file.path(inpath, 'gis')
   fns = c(
-    indir, outdir, gisdir, 
+    inpath, outpath, gisdir, 
     fn.mesh, fn.pt, fn.edge, fn.mdseg, fn.mdriv, fn.att,
     fn.ic,fn.para, fn.calib,
-          fn.soil, fn.geol, fn.lc,
-          fn.forc, fn.bc, fn.lai, fn.rl, fn.mf
-  )
+    fn.soil, fn.geol, fn.lc,
+    fn.forc, fn.bc, fn.lai, fn.rl, fn.mf)
   names(fns) = c(
     'inpath', 'outpath', 'gispath',
     "md.mesh","md.pt", "md.edge", "md.rivseg", "md.riv", "md.att",
